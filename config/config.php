f4ac789079b3b1acdb7dba0c3825efc71c7abd04
Bapel <47784359+bapelindo@users.noreply.github.com>
2025-12-07 11:33:26 +0700

    Change database name for local development

--- config/config.php
+++ config/config.php
@@ -6,7 +6,7 @@
 define('DB_HOST', getenv('DB_HOST') ?: '28s-br.h.filess.io'); // Atau host DB Anda
 define('DB_USER', getenv('DB_USER') ?: 'staywithme_plannedam');      // User DB
 define('DB_PASS', getenv('DB_PASS') ?: 'b604590cb2c93d0c2c78a0e3ae56dbb65f4c1728');          // Password DB
-define('DB_NAME', getenv('DB_NAME') ?: 'staywithme_scientist'); // Nama Database
+define('DB_NAME', getenv('DB_NAME') ?: 'staywithme_plannedam'); // Nama Database
 define('DB_PORT', getenv('DB_PORT') ?: '61030');          // Port Database
 
 // URL dasar aplikasi, dinamis untuk Vercel dan lokal
